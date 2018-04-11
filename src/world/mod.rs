06694264ddf89aac0553e2a9d0c89830fa0aaf99
Rob Taussig <robert.taussig@gmail.com>
2018-04-11 10:21:22 -0400

    Expand information provided by describing world

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,16 +1,19 @@
 mod country;
 
 pub struct World {
-    pub size: u32,
+    pub world_size: u32,
     pub countries: Vec<country::Country>
 }
 
 impl World {
     pub fn generate_countries(number_of_countries: u32, number_of_villages: u32, number_of_villagers: u32) -> Vec<country::Country> {
         let mut created_countries = Vec::new();
-        for _i in 1..number_of_countries {
+        for i in 1..number_of_countries + 1 {
             let mut created_country = country::Country {
-                villages: country::Country::generate_villages(number_of_villages, number_of_villagers)
+                villages: country::Country::generate_villages(number_of_villages, number_of_villagers),
+                environment: country::Country::generate_environment(),
+                name: country::Country::generate_name(),
+                world_location: i
             };
             created_countries.push(created_country);
         }
@@ -19,11 +22,21 @@ impl World {
 
     pub fn describe(&self) {
         for ctry in &self.countries {
+            println!("Country: {}", ctry.name);
+            println!("World Location: {}", ctry.world_location);
+            println!("Environment: [{}]", ctry.environment);
+            println!("----------");
             for village in &ctry.villages {
+                println!("Village: {}", village.name);
+                println!("Country Location: {}", village.country_location);
+                println!("----------");
                 for villager in &village.people {
                     println!("Name: {}", villager.name);
                     println!("Age: {}", villager.age);
-                    println!("Genetics: {}", villager.genetics);
+                    println!("Genetics: [{}]", villager.genetics);
+                    println!("Home: {}, {}", village.name, ctry.name);
+                    println!("Village Location: {}", villager.village_location);
+                    println!("");
                 }
             }
         }
