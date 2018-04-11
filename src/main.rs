7acf259c1436e87dbd15e11c39e3dd983dce93ce
Rob Taussig <robert.taussig@gmail.com>
2018-04-11 10:21:07 -0400

    change property of world.size to world.world_size

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate rand;
+extern crate names;
 mod world;
 
 const WORLD_SIZE: u32 = 5;
@@ -8,7 +9,7 @@ const NUMBER_OF_VILLAGERS_PER_VILLAGE: u32 = 20;
 
 fn main() {
     let created_world = world::World {
-        size: WORLD_SIZE,
+        world_size: WORLD_SIZE,
         countries: world::World::generate_countries(NUMBER_OF_COUNTRIES_PER_WORLD, NUMBER_OF_VILLAGES_PER_COUNTRY, NUMBER_OF_VILLAGERS_PER_VILLAGE)
     };
     created_world.describe();
